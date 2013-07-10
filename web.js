9b434aaeb67ba7c31ae30db42cc3a13975778f2a
Dhouard <dhouard@gmail.com>
2013-07-10 10:49:47 +0200

    uso de buffer

--- web.js
+++ web.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var fs = require('fs');
 
+var buffer = new buffer(fs.readFileSync('index.html'), 'utf-8');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync('index.html'));
+  response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
